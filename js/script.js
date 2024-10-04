d6c10c22a50220f145c2641b192284ef737b1bcf
Yewantraj Karki <54407001+yewant@users.noreply.github.com>
2024-10-04 00:55:29 +0000

    resposive css for small screen fixed and contents added

--- js/script.js
+++ js/script.js
@@ -22,3 +22,20 @@ sr.reveal('.about-text',{delay:200, origin:'top'});
 sr.reveal('.about-img',{delay:450, origin:'top'});
 
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var scrollBtn = document.getElementById("scrollBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
